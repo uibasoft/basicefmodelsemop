49e563e16f59907687b4974e95cd4b50edecd425
Luis Alberto Baigorria Rodas <uialberto@outlook.com>
2016-05-31 12:25:19 -0400

    Se creó el controlador SubAlcaldia con la acción crear, con las vistas correspondientes

---
+++ EFSemop/efsemop/Controllers/SubAlcaldiaController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity.Infrastructure;
+using efsemop.Framework.Pepemosca.Data;
+
+namespace efsemop.Controllers
+{
+    public class SubAlcaldiaController : Controller
+    {
+        // GET: SubAlcaldia
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SubAlcaldia/Crear
+        public ActionResult Crear()
+        {                        
+            return View();
+        }
+
+        // POST: SubAlcaldia/Crear
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Crear([Bind(Include = "IdSubAlcaldia,Nombre,Direccion,Zona,Telefono, NombreSubAlcalde")] SubAlcaldia subAlcaldia)
+        {
+            if (!ModelState.IsValid) return View(subAlcaldia);
+            using (var db = new AlcaldiaModelContainer())
+            {
+                db.SubAlcaldias.Add(subAlcaldia);
+                await db.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
