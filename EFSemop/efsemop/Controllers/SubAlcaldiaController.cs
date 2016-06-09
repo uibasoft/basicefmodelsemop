038c9b4ec10d08d08e9e35870128c25e7ebc4f58
uialberto <uialberto@gmail.com>
2016-06-08 22:37:08 -0400

    Desacomplando componentes!  01 Extraer código de creación y liberación de dependencias

--- EFSemop/efsemop/Controllers/SubAlcaldiaController.cs
+++ EFSemop/efsemop/Controllers/SubAlcaldiaController.cs
@@ -15,6 +15,19 @@ namespace efsemop.Controllers
 {
     public class SubAlcaldiaController : Controller
     {
+        private readonly AlcaldiaModelContainer _db;
+
+        public SubAlcaldiaController()
+        {
+            _db = new AlcaldiaModelContainer();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                _db.Dispose();
+        }
+
         // GET: SubAlcaldia
         public ActionResult Index(string texto, string sortOrder, string currentFilter, int? page)
         {
@@ -33,35 +46,31 @@ namespace efsemop.Controllers
 
             ViewBag.CurrentFilter = texto;
 
-            using (var db = new AlcaldiaModelContainer())
+            var list = from ele in _db.SubAlcaldias
+                       select ele;
+
+            if (!string.IsNullOrEmpty(texto))
             {
-                var list = from ele in db.SubAlcaldias
-                           select ele;
-
-                if (!string.IsNullOrEmpty(texto))
-                {
-                    list = list.Where(s => s.Nombre.Contains(texto) || s.Direccion.Contains(texto));
-                }
-                switch (sortOrder)
-                {
-                    case "nombre_desc":
-                        list = list.OrderByDescending(s => s.Nombre);
-                        break;
-                    case "Zona":
-                        list = list.OrderBy(s => s.Zona);
-                        break;
-                    case "zona_desc":
-                        list = list.OrderByDescending(s => s.Zona);
-                        break;
-                    default:
-                        list = list.OrderBy(s => s.Nombre);
-                        break;
-                }
-                var pageSize = 3;
-                var pageNumber = (page ?? 1);
-                return View(list.ToPagedList(pageNumber, pageSize));
-
+                list = list.Where(s => s.Nombre.Contains(texto) || s.Direccion.Contains(texto));
             }
+            switch (sortOrder)
+            {
+                case "nombre_desc":
+                    list = list.OrderByDescending(s => s.Nombre);
+                    break;
+                case "Zona":
+                    list = list.OrderBy(s => s.Zona);
+                    break;
+                case "zona_desc":
+                    list = list.OrderByDescending(s => s.Zona);
+                    break;
+                default:
+                    list = list.OrderBy(s => s.Nombre);
+                    break;
+            }
+            var pageSize = 3;
+            var pageNumber = (page ?? 1);
+            return View(list.ToPagedList(pageNumber, pageSize));
         }
 
         // GET: SubAlcaldia/Crear
@@ -76,20 +85,19 @@ namespace efsemop.Controllers
         public async Task<ActionResult> Crear([Bind(Include = "Nombre,Direccion,Zona,Telefono, NombreSubAlcalde")] CreateSubAlcaldiaViewModel model)
         {
             if (!ModelState.IsValid) return View(model);
-            using (var db = new AlcaldiaModelContainer())
+
+            var subAlcaldia = new SubAlcaldia()
             {
-                var subAlcaldia = new SubAlcaldia()
-                {
-                    Nombre = model.Nombre,
-                    Direccion = model.Direccion,
-                    Zona = model.Zona,
-                    Telefono = model.Telefono,
-                    NombreSubAlcalde = model.NombreSubAlcalde
-                };
-                db.SubAlcaldias.Add(subAlcaldia);
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
+                Nombre = model.Nombre,
+                Direccion = model.Direccion,
+                Zona = model.Zona,
+                Telefono = model.Telefono,
+                NombreSubAlcalde = model.NombreSubAlcalde
+            };
+            _db.SubAlcaldias.Add(subAlcaldia);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+
 
         }
 
@@ -100,28 +108,27 @@ namespace efsemop.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            using (var db = new AlcaldiaModelContainer())
-            {
-                var subAlcaldia = await db.SubAlcaldias.FindAsync(id);
-                if (subAlcaldia == null)
-                {
-                    if (concurrencyError.GetValueOrDefault())
-                    {
-                        return RedirectToAction("Index");
-                    }
-                    return HttpNotFound();
-                }
 
+            var subAlcaldia = await _db.SubAlcaldias.FindAsync(id);
+            if (subAlcaldia == null)
+            {
                 if (concurrencyError.GetValueOrDefault())
                 {
-                    ViewBag.ConcurrencyErrorMessage = "El registro que intentó eliminar fue modificado por otro usuario después de consultar " +
-                                                      "los valores originales. " +
-                                                      "La operación de eliminación fue cancelada y los valores actuales se han vuelto a " +
-                                                      "cargar. Si aún desea eliminar este registro, haga clic en el botón " +
-                                                      "Eliminar de nuevo. De lo contrario Haga clic en el hipervínculo Cancelar.";
+                    return RedirectToAction("Index");
                 }
-                return View(subAlcaldia);
+                return HttpNotFound();
             }
+
+            if (concurrencyError.GetValueOrDefault())
+            {
+                ViewBag.ConcurrencyErrorMessage = "El registro que intentó eliminar fue modificado por otro usuario después de consultar " +
+                                                  "los valores originales. " +
+                                                  "La operación de eliminación fue cancelada y los valores actuales se han vuelto a " +
+                                                  "cargar. Si aún desea eliminar este registro, haga clic en el botón " +
+                                                  "Eliminar de nuevo. De lo contrario Haga clic en el hipervínculo Cancelar.";
+            }
+            return View(subAlcaldia);
+
         }
 
         // POST: SubAlcaldia/Eliminar/5
@@ -131,13 +138,9 @@ namespace efsemop.Controllers
         {
             try
             {
-                using (var db = new AlcaldiaModelContainer())
-                {
-                    db.Entry(subAlcaldia).State = EntityState.Deleted;
-                    await db.SaveChangesAsync();
-                    return RedirectToAction("Index");
-                }
-
+                _db.Entry(subAlcaldia).State = EntityState.Deleted;
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
             }
             catch (DbUpdateConcurrencyException)
             {
@@ -160,15 +163,12 @@ namespace efsemop.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            using (var db = new AlcaldiaModelContainer())
+            var subAlcaldia = await _db.SubAlcaldias.FindAsync(id);
+            if (subAlcaldia == null)
             {
-                var subAlcaldia = await db.SubAlcaldias.FindAsync(id);
-                if (subAlcaldia == null)
-                {
-                    return HttpNotFound();
-                }
-                return View(subAlcaldia);
+                return HttpNotFound();
             }
+            return View(subAlcaldia);
         }
 
         // POST: SubAlcaldia/Editar/5
@@ -183,59 +183,58 @@ namespace efsemop.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             SubAlcaldia subAlcaldiaToUpdate;
-            using (var db = new AlcaldiaModelContainer())
-            {
-                subAlcaldiaToUpdate = await db.SubAlcaldias.FindAsync(id);
-                if (subAlcaldiaToUpdate == null)
-                {
-                    var deletedSubAlcaldia = new SubAlcaldia();
-                    TryUpdateModel(deletedSubAlcaldia, fieldsToBind);
-                    ModelState.AddModelError(string.Empty, "Imposible guardar los cambios. El elemento fué eliminado por otro usuario.");
-                    return View(deletedSubAlcaldia);
-                }
-                if (!TryUpdateModel(subAlcaldiaToUpdate, fieldsToBind)) return View(subAlcaldiaToUpdate);
-                try
-                {
-                    await db.SaveChangesAsync();
-                    return RedirectToAction("Index");
-                }
-                catch (DbUpdateConcurrencyException ex)
-                {
-                    var entry = ex.Entries.Single();
-                    var clientValues = (SubAlcaldia)entry.Entity;
-                    var databaseEntry = entry.GetDatabaseValues();
-                    if (databaseEntry == null)
-                    {
-                        ModelState.AddModelError(string.Empty, "Imposible guardar los cambios. El elemento fué eliminado por otro usuario.");
-                    }
-                    else
-                    {
-                        var databaseValues = (SubAlcaldia)databaseEntry.ToObject();
 
-                        if (databaseValues.Nombre != clientValues.Nombre)
-                            ModelState.AddModelError("Nombre", "Nombre valor: "
-                                                               + databaseValues.Nombre);
-                        if (databaseValues.Direccion != clientValues.Direccion)
-                            ModelState.AddModelError("Direccion", "Direccion valor: "
-                                                                  + databaseValues.Nombre);
-                        if (databaseValues.Zona != clientValues.Zona)
-                            ModelState.AddModelError("Zona", "Zona valor: "
-                                                             + databaseValues.Zona);
-                        if (databaseValues.Telefono != clientValues.Telefono)
-                            ModelState.AddModelError("Telefono", "Telefono valor: "
-                                                                 + databaseValues.Telefono);
-                        if (databaseValues.NombreSubAlcalde != clientValues.NombreSubAlcalde)
-                            ModelState.AddModelError("NombreSubAlcalde", "NombreSubAlcalde valor: "
-                                                                         + databaseValues.NombreSubAlcalde);
-                        ModelState.AddModelError(string.Empty, "El registro que está intentado modificar, fue eliminado por otro usuario después de consultar " +
-                                                          "los valores originales. La operación de edición fue cancelada.");
-                    }
-                }
-                catch (RetryLimitExceededException)
-                {
-                    ModelState.AddModelError("", "No se puede editar. Inténtelo de nuevo, y si el problema persiste pongase con el administrador del sistema.");
-                }               
+            subAlcaldiaToUpdate = await _db.SubAlcaldias.FindAsync(id);
+            if (subAlcaldiaToUpdate == null)
+            {
+                var deletedSubAlcaldia = new SubAlcaldia();
+                TryUpdateModel(deletedSubAlcaldia, fieldsToBind);
+                ModelState.AddModelError(string.Empty, "Imposible guardar los cambios. El elemento fué eliminado por otro usuario.");
+                return View(deletedSubAlcaldia);
             }
+            if (!TryUpdateModel(subAlcaldiaToUpdate, fieldsToBind)) return View(subAlcaldiaToUpdate);
+            try
+            {
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                var entry = ex.Entries.Single();
+                var clientValues = (SubAlcaldia)entry.Entity;
+                var databaseEntry = entry.GetDatabaseValues();
+                if (databaseEntry == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Imposible guardar los cambios. El elemento fué eliminado por otro usuario.");
+                }
+                else
+                {
+                    var databaseValues = (SubAlcaldia)databaseEntry.ToObject();
+
+                    if (databaseValues.Nombre != clientValues.Nombre)
+                        ModelState.AddModelError("Nombre", "Nombre valor: "
+                                                           + databaseValues.Nombre);
+                    if (databaseValues.Direccion != clientValues.Direccion)
+                        ModelState.AddModelError("Direccion", "Direccion valor: "
+                                                              + databaseValues.Nombre);
+                    if (databaseValues.Zona != clientValues.Zona)
+                        ModelState.AddModelError("Zona", "Zona valor: "
+                                                         + databaseValues.Zona);
+                    if (databaseValues.Telefono != clientValues.Telefono)
+                        ModelState.AddModelError("Telefono", "Telefono valor: "
+                                                             + databaseValues.Telefono);
+                    if (databaseValues.NombreSubAlcalde != clientValues.NombreSubAlcalde)
+                        ModelState.AddModelError("NombreSubAlcalde", "NombreSubAlcalde valor: "
+                                                                     + databaseValues.NombreSubAlcalde);
+                    ModelState.AddModelError(string.Empty, "El registro que está intentado modificar, fue eliminado por otro usuario después de consultar " +
+                                                      "los valores originales. La operación de edición fue cancelada.");
+                }
+            }
+            catch (RetryLimitExceededException)
+            {
+                ModelState.AddModelError(string.Empty, "No se puede editar. Inténtelo de nuevo, y si el problema persiste pongase con el administrador del sistema.");
+            }
+
             return View(subAlcaldiaToUpdate);
         }
 
---
+++ EFSemop/efsemop/Framework/Pepemosca/Data/AlcaldiaModelContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace efsemop.Framework.Pepemosca.Data
+{
+    public partial class AlcaldiaModelContainer : IModelContainer
+    {
+    }
+}
